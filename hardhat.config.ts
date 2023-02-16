0c0029ac937dd3d3a8cbc097ce1a51a03468d1d2
superman <goodnesskolapo@gmail.com>
2023-02-16 19:40:53 +0100

    omo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  networks: {
+
+      hardhat: {
+        
+        forking: {
+          //@ts-ignore
+          url: process.env.MAINETURL,
+        }
+      },
+  },
+  etherscan: {
+    //@ts-ignore
+    api_key: process.env.ETHERSCAN_API_KEY,
+},
+
+
 };
 
 export default config;
