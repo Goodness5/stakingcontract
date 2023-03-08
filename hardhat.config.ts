bbc37004e2a746a5adaec7ea40d2af220fe48f8d
superman <goodnesskolapo@gmail.com>
2023-03-08 04:04:19 +0100

    omo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,25 +7,30 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
 
   networks: {
-    goerli: {
-      url: process.env.GOERLI_RPC,
-      //@ts-ignore
-      accounts: [process.env.PRIVATE_KEY],
-    },
 
-    hardhat: {
-      forking: {
-        //@ts-ignore
-        url: process.env.MAINETURL,
+      hardhat: {
+        
+        forking: {
+          enabled: true,
+          //@ts-ignore
+          url: process.env.MAINETURL,
+        }
       },
-    },
+      goerli: {
+        url: process.env.GOERLI_RPC,
+        //@ts-ignore
+        accounts: [process.env.PRIVATE_KEY],
+      },
   },
   etherscan: {
     //@ts-ignore
     api_key: process.env.ETHERSCAN_API_KEY,
-  },
+},
+
+
 };
 
 export default config;
